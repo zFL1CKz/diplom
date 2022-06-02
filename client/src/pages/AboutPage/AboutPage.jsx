a98b60c2da7224f27c59cdcfa719011ed2a82ea7
zFL1CKz <dimakukharev2002@yandex.ru>
2022-06-02 20:28:07 +0300

    added companys info

--- client/src/pages/AboutPage/AboutPage.jsx
+++ client/src/pages/AboutPage/AboutPage.jsx
@@ -25,10 +25,11 @@ export const AboutPage = () => {
       <div className='about'>
         <div className='about__block'>
           <div className='about__title'>Что такое MotoSoul?</div>
+          <div className='about__subtitle'></div>
           <div className='about__text'>
             <span>MotoSoul</span> — это компания, которая предоставляет возможность аренды любого мотоцикла для абсолютно разных задач (езда по городу и загороду, практика на колецевом автодроме или
-            по бездорожью). <br />
-            <br /> Центральный офис компании находится по адресу: Россия, Москва, Тверская улица, 7. Адреса точек для аренды представлены на карте ниже.
+            по бездорожью) <br />
+            <br /> Центральный офис компании находится по адресу: Россия, Москва, Тверская улица, 7. Точки для аренды мотоциклов указаны ниже на карте
           </div>
 
           <iframe
@@ -39,19 +40,76 @@ export const AboutPage = () => {
         </div>
       </div>
 
-      <div className='terms__wrapper'>
-        <Link to='/terms/contract' className='terms__item'>
-          Договор
-        </Link>
-        <Link to='/terms/rent-contract' className='terms__item'>
-          Договор аренды мотоцикла
-        </Link>
-        <Link to='/terms/user-agreement' className='terms__item'>
-          Пользовательское соглашение
-        </Link>
-        <Link to='/terms/privacy' className='terms__item'>
-          Политика в отношении обработки персональных данных
-        </Link>
+      <div className='about__block'>
+        <div className='about__title'>Контакты</div>
+        <div className='about__subtitle'></div>
+
+        <div className='about__text'>
+          <span>По вопросам, связанным с работой сервиса: </span>
+          +7 495 664-87-87 <br /> <br />
+          <span>Центральный офис:</span> <br />
+          Россия, Москва, Тверская улица, 7. <br />
+          Ежедневно с 09:00 до 18:00 <br />
+          +7 495 664-87-87 <br />
+          <br />
+          <span>Банковские реквизиты:</span> <br />
+          Наименование клиента: ООО "МотоСоул"
+          <br />
+          <br />
+          Наименование клиента латинискими буквами: LLC "MotoSoul"
+          <br />
+          <br />
+          Юридический адрес: 125375, г. Москва, ул. Тверская улица, дом 7.
+          <br />
+          <br />
+          ИНН: 1234567890
+          <br />
+          <br />
+          КПП: 771292983
+          <br />
+          <br />
+          ОГРН: 1145638208083
+          <br />
+          <br />
+          E-mail: motosoul@gmail.com
+          <div className='about__subtitle'></div>
+          Наименование банка получателя: АО «РАЙФФАЙЗЕНБАНК»
+          <br />
+          <br />
+          Расчетный счет: 40702810300000004404
+          <br />
+          <br />
+          Корреспондентский счет: 30101810200000000700
+          <br />
+          <br />
+          ИНН: 7744000302
+          <br />
+          <br />
+          КПП: 770201001
+          <br />
+          <br />
+          БИК: 044525700
+        </div>
+      </div>
+
+      <div className='about__block'>
+        <div className='about__title'>Документы</div>
+        <div className='about__subtitle'></div>
+
+        <div className='terms__wrapper'>
+          <Link to='/terms/contract' className='terms__item'>
+            Договор
+          </Link>
+          <Link to='/terms/rent-contract' className='terms__item'>
+            Договор аренды мотоцикла
+          </Link>
+          <Link to='/terms/user-agreement' className='terms__item'>
+            Пользовательское соглашение
+          </Link>
+          <Link to='/terms/privacy' className='terms__item'>
+            Политика в отношении обработки персональных данных
+          </Link>
+        </div>
       </div>
     </div>
   )
